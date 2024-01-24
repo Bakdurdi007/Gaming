72d95ecafb82328471f6cdd8991dc4f10e23bc78
User <abdusolih1241@gmail.com>
2024-01-24 17:18:30 +0200

    Hello Zaxro

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,6 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Zaxro Hello");
         Menu();
     }
 
